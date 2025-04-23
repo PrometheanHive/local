460163416b5039a3d19cebc9f2c33f166294a6e7
Promethean Hive <prometheanhive@gmail.com>
2025-04-23 16:51:15 +0000

    convos show up right

--- frontend/src/auth/AuthProvider.tsx
+++ frontend/src/auth/AuthProvider.tsx
@@ -8,6 +8,19 @@ interface AuthContextType {
   isLoading: boolean;
 }
 
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  bio?: string;
+  profile_pic?: string;
+  is_traveler: boolean;
+  is_host: boolean;
+}
+
+
 // Create the AuthContext with a default value
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
@@ -27,7 +40,7 @@ interface AuthProviderProps {
 
 // AuthProvider Component
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
