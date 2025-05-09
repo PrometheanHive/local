4e00810805eb830c82b36f9a2e67f4c5ec8a3076
Promethean Hive <prometheanhive@gmail.com>
2025-05-09 04:40:15 +0000

    Location foundation updates

--- backend/general/models.py
+++ backend/general/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 from django.utils.timezone import now
 from django.contrib.auth.models import AbstractUser
+from django.contrib.gis.db import models as geomodels 
 
 class EventTags(models.Model):
     tag_name = models.CharField(max_length=200, unique=True)
@@ -21,11 +22,17 @@ class Event(models.Model):
     host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name='hosted_events')
     created_at = models.DateTimeField(default=now)
     number_of_guests = models.PositiveIntegerField(default=1, blank=True, null=True)
-    number_of_bookings = models.PositiveIntegerField(default=0, blank=False, null=False)
+    number_of_bookings = models.PositiveIntegerField(default=0)
     occurence_date = models.DateTimeField(blank=True, null=True)
     duration = models.DurationField(blank=True, null=True)
-    location = models.CharField(max_length=255, blank=True, null=True)  # Improved location field
-    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2, blank=True, null=True)  # Improved price field
+
+    # NEW GEO INFO
+    location = models.CharField(max_length=255)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    point = geomodels.PointField(geography=True, blank=True, null=True)
+
+    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2, blank=True, null=True)
     photos = models.JSONField(blank=True, null=True)
 
     def __str__(self):
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
-import { NavigationBar } from './components/NavigationBar/NavigationBar';
+import { LoadScript } from '@react-google-maps/api';
 
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; // <-- put in your .env
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Router />
-    </MantineProvider>
+    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={['places']}>
+      <MantineProvider theme={theme}>
+        <Router />
+      </MantineProvider>
+    </LoadScript>
   );
 }
