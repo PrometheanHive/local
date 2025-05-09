bd5dca6bef356289a8bdcd855181f02f43e19bb8
Promethean Hive <prometheanhive@gmail.com>
2025-05-09 05:39:41 +0000

    start payment integration

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
+STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
 
 DOMAIN = os.getenv("DOMAIN", "localhost")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
