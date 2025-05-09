684048c4188d544690301c4ff35186946fa115a4
Promethean Hive <prometheanhive@gmail.com>
2025-05-09 19:24:33 +0000

    added  maps package to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,17 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
-import { LoadScript } from '@react-google-maps/api';
+import { LoadScriptNext } from '@react-google-maps/api';
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; // <-- put in your .env
 
 export default function App() {
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={['places']}>
+    <LoadScriptNext googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={['places']}>
       <MantineProvider theme={theme}>
         <Router />
       </MantineProvider>
-    </LoadScript>
+    </LoadScriptNext>
   );
 }
+
