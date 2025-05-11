3676c46a815bc7b03811146766541952313325dd
Promethean Hive <prometheanhive@gmail.com>
2025-05-11 17:24:19 +0000

    removed duplicated api key error.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,11 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
-import { LoadScriptNext } from '@react-google-maps/api';
-
-const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; // <-- put in your .env
 
 export default function App() {
   return (
-    <LoadScriptNext googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={['places']}>
-      <MantineProvider theme={theme}>
-        <Router />
-      </MantineProvider>
-    </LoadScriptNext>
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
   );
 }
-
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './auth/AuthProvider';
 
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&v=beta`;
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AuthProvider>
     <App />
